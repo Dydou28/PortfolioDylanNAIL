7d1073e7d088026d0765f647216b8bb686a70a65
Dylan <dylannail@orange.fr>
2024-01-08 16:17:33 +0100

    mise à jour couleur

--- frontend/src/pages/Competences/Competences.jsx
+++ frontend/src/pages/Competences/Competences.jsx
@@ -1,34 +1,34 @@
 import "./Competences.scss";
 
-import { FaReact } from "react-icons/fa";
-import { FaNodeJs } from "react-icons/fa";
-import { BsGit } from "react-icons/bs";
-import { SiMysql } from "react-icons/si";
-import { FaAngular } from "react-icons/fa";
-import { SiCanva } from "react-icons/si";
+// import { FaReact } from "react-icons/fa";
+// import { FaNodeJs } from "react-icons/fa";
+// import { BsGit } from "react-icons/bs";
+// import { SiMysql } from "react-icons/si";
+// import { FaAngular } from "react-icons/fa";
+// import { SiCanva } from "react-icons/si";
 
-export default function Competences() {
-  return (
-    <main className="competences">
-      <div className="bloc-comp">
-        <div className="logo1-comp">
-          <h2 className="titre-comp">Compétences</h2>
-          <div className="logo-comp">
-            <div className="flex1">
-              <FaReact className="logo" />
-              <FaNodeJs className="logo" />
-            </div>
-            <div className="flex2">
-              <BsGit className="logo" />
-              <SiMysql className="logo" />
-            </div>
-            <div className="flex3">
-              <FaAngular className="logo" />
-              <SiCanva className="logo" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-}
+// export default function Competences() {
+//   return (
+//     <main className="competences">
+//       <div className="bloc-comp">
+//         <div className="logo1-comp">
+//           <h2 className="titre-comp">Compétences</h2>
+//           <div className="logo-comp">
+//             <div className="flex1">
+//               <FaReact className="logo" />
+//               <FaNodeJs className="logo" />
+//             </div>
+//             <div className="flex2">
+//               <BsGit className="logo" />
+//               <SiMysql className="logo" />
+//             </div>
+//             <div className="flex3">
+//               <FaAngular className="logo" />
+//               <SiCanva className="logo" />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
